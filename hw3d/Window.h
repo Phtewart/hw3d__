f3f45cd562415e36eb58ab74336cde90a7158167
Phtewart <moimidlo@mail.ru>
2023-02-08 22:07:03 +0300

    added mouse class

--- hw3d/Window.h
+++ hw3d/Window.h
@@ -2,6 +2,7 @@
 #include "CustmWin.h"
 #include "CustmException.h"
 #include "Keyboard.h"
+#include "Mouse.h"
 
 
 class Window
@@ -45,6 +46,7 @@ private:
 	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
 public:
 	Keyboard kbd;
+	Mouse mouse;
 private:
 	int width;
 	int height;
