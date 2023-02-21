a4020dee9721bf10dabf19840f1946c4000abcfe
Phtewart <moimidlo@mail.ru>
2023-02-21 22:01:58 +0300

    add mesh root node system. work fine

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -17,62 +17,7 @@ App::App()
 	pLight(wnd.Gfx())
 	
 {
-	class Factory
-	{
-	public:
-		Factory(Graphics& gfx)
-			:
-			gfx(gfx)
-		{}
-		std::unique_ptr<Drawable> operator()()
-		{
-			const DirectX::XMFLOAT3 mat = { cdist(rng),cdist(rng),cdist(rng) };
-
-			switch (typedist(rng))
-			{
-			case 0:
-				return std::make_unique<Box>(
-					gfx, rng, adist, ddist,
-					odist, rdist, bdist, mat
-					);
-			case 1:
-				return std::make_unique<AssimpM>(
-					gfx, rng, adist, ddist,
-					odist, rdist,mat,1.5f
-					);
-			default:
-				assert(false && "bad drawable type in factory");
-				return {};
-			}
-		}
-	private:
-		float PI = 3.14f;
-		Graphics& gfx;
-		std::mt19937 rng{ std::random_device{}() };
-		std::uniform_real_distribution<float> adist{ 0.0f,PI * 2.0f };
-		std::uniform_real_distribution<float> ddist{ 0.0f,PI * 0.5f };
-		std::uniform_real_distribution<float> odist{ 0.0f,PI * 0.08f };
-		std::uniform_real_distribution<float> rdist{ 6.0f,20.0f };
-		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
-		std::uniform_int_distribution<int> latdist{ 5,20 };
-		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_real_distribution<float> cdist{ 0.0f,1.0f };;
-		std::uniform_int_distribution<int> typedist{ 0,1 };
-	};
-
-	drawables.reserve(nDrawables);
-	std::generate_n(std::back_inserter(drawables), nDrawables, Factory{ wnd.Gfx() });
-
-	// init box pointers for editing instance parameters
-	for (auto& pd : drawables)
-	{
-		if (auto pb = dynamic_cast<Box*>(pd.get()))
-		{
-			boxes.push_back(pb);
-		}
-	}
-
-	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
+	wnd.Gfx().SetProjection(dx::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
 
 void App::DoFrame()
@@ -82,79 +27,16 @@ void App::DoFrame()
 	wnd.Gfx().SetCamera(camera.GetMatrix());
 	pLight.Bind(wnd.Gfx(),camera.GetMatrix());
 
-	for (auto& d : drawables)
-	{
-		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
-		d->Draw(wnd.Gfx());
-	}
+	nano.Draw(wnd.Gfx());
 	pLight.Draw(wnd.Gfx());
-
+	
 	//imgui window
-	SpawnSimulationWindow();
 	camera.SpawnControlWindow();
 	pLight.SpawnControlWindow();
-	SpawnBoxWindowManagerWindow();
-	SpawnBoxWindows();
+	nano.ShowWindow();
 
 	// present
 	wnd.Gfx().EndFrame();
-}
-
-void App::SpawnSimulationWindow() noexcept
-{
-	if (ImGui::Begin("Simulation Speed"))
-	{
-		ImGui::SliderFloat("Speed Factor", &speed_factor, 0.0f, 6.0f, "%.4f"); 
-		ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::Text("Status: %s", wnd.kbd.KeyIsPressed(VK_SPACE) ? "PAUSED" : "RUNNING (hold spacebar to pause)");
-	}
-	ImGui::End();
-}
-
-void App::SpawnBoxWindowManagerWindow() noexcept
-{
-	if (ImGui::Begin("Boxes"))
-	{
-		using namespace std::string_literals;
-		const auto preview = comboBoxIndex ? std::to_string(*comboBoxIndex) : "Choose a box..."s;
-		if (ImGui::BeginCombo("Box Number", preview.c_str()))
-		{
-			for (int i = 0; i < boxes.size(); i++)
-			{
-				const bool selected = comboBoxIndex == i;
-				if (ImGui::Selectable(std::to_string(i).c_str(), selected))
-				{
-					comboBoxIndex = i;
-				}
-				if (selected)
-				{
-					ImGui::SetItemDefaultFocus();
-				}
-			}
-			ImGui::EndCombo();
-		}
-		if (ImGui::Button("Spawn Control Window") && comboBoxIndex)
-		{
-			boxControlIds.insert(*comboBoxIndex);
-			comboBoxIndex.reset();
-		}
-	}
-	ImGui::End();
-}
-
-void App::SpawnBoxWindows() noexcept
-{
-	for (auto i = boxControlIds.begin(); i != boxControlIds.end(); )
-	{
-		if (!boxes[*i]->SpawnControlWindow(*i, wnd.Gfx()))
-		{
-			i = boxControlIds.erase(i);
-		}
-		else
-		{
-			i++;
-		}
-	}
 }
 
 App::~App()
